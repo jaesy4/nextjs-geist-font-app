c757212ee716886a31eb7ab9f79cdf957bfa7d3a
jaesy4 <106516400+jaesy4@users.noreply.github.com>
2025-08-01 16:13:25 +0800

    Add .next/static/chunks/src_app_admin_dashboard_page_tsx_80bdbab5._.js 1754036005274

---
+++ .next/static/chunks/src_app_admin_dashboard_page_tsx_80bdbab5._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_eedf246e._.js",
+  "static/chunks/node_modules_42ef9e50._.js"
+],
+    source: "dynamic"
+});
